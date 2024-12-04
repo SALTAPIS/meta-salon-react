a43a4468dc50757d75491ca6ca341544f81321b3
SALTAPIS <salontapis@gmail.com>
2024-12-04 20:07:27 +0100

    feat: add React Query provider to fix dashboard data fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import { RouterProvider } from 'react-router-dom';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { AuthProvider } from './components/auth/AuthProvider';
 import { router } from './routes';
+import { theme } from './theme';
+
+const queryClient = new QueryClient();
 
 export default function App() {
   return (
-    <ChakraProvider>
-      <AuthProvider>
-        <RouterProvider router={router} />
-      </AuthProvider>
-    </ChakraProvider>
+    <>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <ChakraProvider theme={theme}>
+        <QueryClientProvider client={queryClient}>
+          <AuthProvider>
+            <RouterProvider router={router} />
+          </AuthProvider>
+        </QueryClientProvider>
+      </ChakraProvider>
+    </>
   );
 }
