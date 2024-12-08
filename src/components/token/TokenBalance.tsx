5defba864deb0784cbe96d658b3fc6e796dbb6ef
SALTAPIS <salontapis@gmail.com>
2024-12-09 00:18:18 +0100

    fix: remove unused React import in TokenBalance

--- src/components/token/TokenBalance.tsx
+++ src/components/token/TokenBalance.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useAuth } from '../../hooks/useAuth';
 import { TokenService } from '../../services/token/tokenService';
 
